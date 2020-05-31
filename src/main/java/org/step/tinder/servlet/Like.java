26b4ecd95ee18c74d30bc643489017bc715d5e44
Ayşən Rzayeva <rza.ayshan@gmail.com>
2020-05-31 13:05:02 +0400

    #3

--- src/main/java/org/step/tinder/servlet/Like.java
+++ src/main/java/org/step/tinder/servlet/Like.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.util.Arrays;
 import java.util.HashMap;
--- src/main/java/org/step/tinder/servlet/Start.java
+++ src/main/java/org/step/tinder/servlet/Start.java
@@ -17,7 +17,6 @@ import java.util.LinkedList;
 public class Start extends HttpServlet {
     private final TemplateEngine engine;
     private final Connection conn;
-
     static LinkedList<Profile> unlikes;
 
     public Start(TemplateEngine engine, Connection conn) {
@@ -26,22 +25,29 @@ public class Start extends HttpServlet {
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        DaoLikes daoLikes = new DaoLikes(conn);
-        String uname = req.getParameter("uname");
-        unlikes = daoLikes.getLikes(uname,false);
+        getUnlikes(req);
         if(unlikes.isEmpty()){
             resp.sendRedirect("/list");
         }
         else{
-            HashMap<String, String> user = new HashMap<>();
-            user.put("uname",unlikes.get(0).getUname());
-            user.put("image",unlikes.get(0).getImage());
-            user.put("name",unlikes.get(0).getName());
-            user.put("surname",unlikes.get(0).getSurname());
-
-
-            engine.render("like-page.ftl", user, resp);
+            HashMap<String, Object> data = createData();
+            engine.render2("like-page.ftl", data, resp);
         }
+    }
+
+    private void getUnlikes(HttpServletRequest req){
+        DaoLikes daoLikes = new DaoLikes(conn);
+        String uname = req.getParameter("uname");
+        unlikes = daoLikes.getLikes(uname,false);
+    }
+
+    private HashMap<String, Object> createData(){
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("uname",unlikes.get(0).getUname());
+        data.put("image",unlikes.get(0).getImage());
+        data.put("name",unlikes.get(0).getName());
+        data.put("surname",unlikes.get(0).getSurname());
+        return data;
     }
 
     @Override
