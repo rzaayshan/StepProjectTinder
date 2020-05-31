581a7f8f4fbd473b5fe9954f3f2c74a75fa056f2
Ayşən Rzayeva <rza.ayshan@gmail.com>
2020-05-31 23:08:39 +0400

    #3

--- src/main/java/org/step/tinder/servlet/StaticServlet.java
+++ src/main/java/org/step/tinder/servlet/StaticServlet.java
@@ -1,11 +1,14 @@
 package org.step.tinder.servlet;
 
+import lombok.SneakyThrows;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,13 +21,15 @@ public class StaticServlet extends HttpServlet {
     this.subPath = subPath;
   }
 
+  @SneakyThrows
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     String filename = req.getPathInfo();
-    String osFileLocation = "content";
-    Path path = Paths.get(osFileLocation, subPath, filename);
+    URL url = this.getClass().getClassLoader().getResource(filename);
+    Path path = Paths.get(url.toURI());
     try (OutputStream os = resp.getOutputStream()) {
       Files.copy(path, os);
+      //Files.copy(this.getClass().getClassLoader().getResource(subPath),os);
     }
   }
 
