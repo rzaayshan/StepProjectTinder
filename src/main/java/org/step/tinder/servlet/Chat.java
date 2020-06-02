b6a24b3db6aa40a81d4fa8825fab18c605ac5c14
Ayşən Rzayeva <rza.ayshan@gmail.com>
2020-06-02 19:06:34 +0400

    #3

--- src/main/java/org/step/tinder/servlet/Chat.java
+++ src/main/java/org/step/tinder/servlet/Chat.java
@@ -1,8 +1,8 @@
 package org.step.tinder.servlet;
 
+import lombok.extern.log4j.Log4j2;
 import org.step.tinder.DAO.DaoMessage;
 import org.step.tinder.DAO.DaoUsers;
-import org.step.tinder.cookies.Crip;
 import org.step.tinder.entity.Message;
 import org.step.tinder.entity.TemplateEngine;
 import org.step.tinder.entity.User;
@@ -10,54 +10,78 @@ import org.step.tinder.entity.User;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.sql.Connection;
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
+@Log4j2
 public class Chat extends HttpServlet {
     private final TemplateEngine engine;
-    private final Connection conn;
+    private final DaoMessage daoMessage;
+    private final DaoUsers daoUsers;
 
     public Chat(TemplateEngine engine, Connection conn) {
         this.engine = engine;
-        this.conn = conn;
+        daoMessage = new DaoMessage(conn);
+        daoUsers = new DaoUsers(conn);
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        HashMap<String, Object> data = new HashMap<>();
-        HttpSession session = req.getSession();
-        String from = (String) session.getAttribute("uname");
-        DaoMessage dao = new DaoMessage(conn);
-        DaoUsers daoUsers = new DaoUsers(conn);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp){
         String to = req.getParameter("to");
-        List<Message> messages = dao.getMessages(from,to);
+        if(daoUsers.get(to).isPresent()){
+            HashMap<String, Object> data = createData(req);
+            engine.render("chat.ftl", data, resp);
+        }
+        else {
+            try {
+                resp.sendRedirect("/list");
+            } catch (IOException exception) {
+                log.error(exception.getMessage());
+            }
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp){
+        String to = req.getParameter("to");
+        if(daoUsers.get(to).isPresent()) {
+            addMessage(req);
+            HashMap<String, Object> data = createData(req);
+            engine.render("chat.ftl", data, resp);
+        }
+        else {
+            try {
+                resp.sendRedirect("/list");
+            } catch (IOException exception) {
+                log.error(exception.getMessage());
+            }
+        }
+    }
+
+    private HashMap<String, Object> createData(HttpServletRequest req){
+        String from = (String) req.getSession().getAttribute("uname");
+        String to = req.getParameter("to");
+        List<Message> messages = daoMessage.getMessages(from,to);
+        String image = daoUsers.get(to).get().getImage();
+
+        HashMap<String, Object> data = new HashMap<>();
+
         data.put("messages", messages);
         data.put("to",to);
         data.put("from",from);
-        data.put("image",daoUsers.get(to).map(User::getImage));
+        data.put("image",image);
 
-        engine.render("chat.ftl", data, resp);
+        return data;
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
-        HashMap<String, Object> data = new HashMap<>();
-        DaoMessage dao = new DaoMessage(conn);
+    private void addMessage(HttpServletRequest req){
+        String from = (String) req.getSession().getAttribute("uname");
+        String to = req.getParameter("to");
         String mes = req.getParameter("mes");
-        String from = req.getParameter("from");
-        String to = req.getParameter("to");
-        dao.put(new Message(from,to,LocalDateTime.now(),mes));
-        List<Message> messages = dao.getMessages(from,to);
-        data.put("messages", messages);
-        data.put("to",to);
-        data.put("from",from);
-
-        engine.render("chat.ftl", data, resp);
-
+        daoMessage.put(new Message(from,to,LocalDateTime.now(),mes));
     }
 }
